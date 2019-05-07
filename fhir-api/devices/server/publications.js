fb0e79d8820a2aca6b6823b4d5908c3a4cd3a1c8
Abigail Watson <awatson1978@gmail.com>
2019-05-06 20:58:55 -0400

    removed api dir

--- fhir-api/devices/server/publications.js
+++
@@ -1,8 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import { Devices } from '../devices';
-
-//Meteor.publish('devices', () => Devices.find());
-
-Meteor.publish('devices', function(){
-  return Devices.find()
-});
